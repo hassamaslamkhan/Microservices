487ec7875bbfba6b22eb8e4f1ce19623a3f70cef
Unknown <hassamaslamkhan@gmail.com>
2018-08-29 22:15:13 -0700

    first commit : hassam

---
+++ src/main/java/com/telenor/rest/RestMicroservice/RestMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.telenor.rest.RestMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMicroserviceApplication.class, args);
+	}
+}
